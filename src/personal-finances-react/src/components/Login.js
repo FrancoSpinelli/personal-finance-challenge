e4adeecdb85a23e5bb8357561ed4baf648c6437f
FrancoSpinelli <franco_spinelli@hotmail.com>
2021-11-22 16:26:45 -0300

    'Register' Component

--- src/personal-finances-react/src/components/Login.js
+++ src/personal-finances-react/src/components/Login.js
@@ -4,16 +4,31 @@ import Title from './Title';
 
 const Login = () => {
     return (
-        <div className="login">
-            <Title name="Sign in"/>
-            <div className="info">
-                <input type="email" placeholder="Mail" autoComplete="email" autoFocus/>
-                <input type="password" placeholder="Password"/>
-                <p id="forgot">Forgot password?</p>
-                <ButtonType type="receipt" name="Sign In"/>
-                <p id="register">You are new? <span className="link">Create an account.</span></p>
-            </div>
-        </div>
+        <React.Fragment>
+            <section className="login">
+                <Title name="Sign in"/>
+                <div className="info">
+                    <input type="email" placeholder="Mail" autoComplete="email" autoFocus/>
+                    <input type="password" placeholder="Password"/>
+                    <p id="forgot">Forgot password?</p>
+                    <ButtonType type="receipt" name="Sign In"/>
+                    <p id="register">You are new? <span className="link">Create an account.</span></p>
+                </div>
+            </section>
+
+            <section className="register">
+                <Title name="Sign up"/>
+                <div className="info">
+                    <input type="text" placeholder="First name" autoComplete="given-name"/>
+                    <input type="text" placeholder="Last name" autoComplete="family-name"/>
+                    <input type="email" placeholder="Mail" autoComplete="email"/>
+                    <input type="password" placeholder="Password"/>
+                    <input type="file"/>
+                    <ButtonType type="receipt" name="Sign up"/>
+                    <p id="register"><span className="link">Already have an account?</span></p>
+                </div>
+            </section>
+        </React.Fragment>
     );
 }
 
